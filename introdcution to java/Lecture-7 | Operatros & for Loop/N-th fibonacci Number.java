80963d4910d1ac4b019d936a24ebcd6487bcf050
Abhi Saxena <115852773+avi1289@users.noreply.github.com>
2023-01-14 17:01:52 +0530

    Update N-th fibonacci Number.java

--- introdcution to java/Lecture-7 | Operatros & for Loop/N-th fibonacci Number.java
+++ introdcution to java/Lecture-7 | Operatros & for Loop/N-th fibonacci Number.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+public class Solution{
+    public static void main(String[] args) {
+        Scanner  sc= new Scanner(System.in);
+        int  number =sc.nextInt();
+        int position =getPosition(number);
+        
+        System.out.print(position);
+        
+    }
+    static int getPosition(int n){
+        if(n==1 || n==2){
+            return 1;
+        }
+        return getPosition(n-1)+getPosition(n-2);
+    }
+}
